20f0e4fca2ed18a367901240160cbbf4462ce8dd
oscar sanchez <00oscarsanchez00@gmail.com>
2024-10-17 19:13:44 -0500

    added moderation folder back in for smaller update to git

---
+++ moderation/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const PORT = 4003;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', (req, res, next) => {
+  const { type, data } = req.body;
+  const event = {};
+  switch (type) {
+    case 'COMMENT_CREATED':
+      (event.type = 'COMMENT_MODERATED'),
+        (event.data = {
+          ...data,
+          status: data.content.includes('warm') ? 'rejected' : 'approved',
+        });
+
+      axios
+        .post('http://bus-srv:4005/events', event)
+        .catch((e) => console.error(e));
+      break;
+    default:
+      '';
+  }
+
+  res.send({ message: 'event received' });
+});
+
+app.listen(PORT, () =>
+  console.log(`moderation server listening on port: ${PORT}`)
+);
